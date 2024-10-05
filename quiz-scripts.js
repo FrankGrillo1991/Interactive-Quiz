99179e8af6ace8336a5a995abf1143d819da0da6
FrankGrillo1991 <fsg12491@aol.com>
2024-10-05 11:44:01 -0400

    Update quiz-scripts.js

--- quiz-scripts.js
+++ quiz-scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const questionElement = document.getElementById('question');
     const optionsElement = document.getElementById('options');
     const nextElement = document.getElementById('next-button');
-    let currentIndex = 0;
+    let currentQuestionIndex = 0;
 
     const questions = [
         {
